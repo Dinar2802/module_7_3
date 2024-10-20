89d2133c9122f45f2adc7824b0ed0d378f78de33
Dinar2802 <dinar.raisovich@mail.ru>
2024-10-20 15:26:30 +0300

    module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -8,39 +8,37 @@ class WordsFinder:
         words = []
         simbols = [',', '.', '=', '!', '?', ';', ':', ' - ']
         for file_name in self.file_names:
-            with open(file_name, 'r', encoding='utf-8') as open_:
-                for line in open_:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = []
+                for line in file:
                     line = line.lower()
-                    for p in simbols:
-                        if p in line:
-                            line = line.replace(p, ' ')
-                    split_line = line.split(sep=' ')
-                    words.append(split_line)
-        sorted_words = [x for y in words for x in y]
-        all_words[self.file_name] = sorted_words
+                    punctuation = [',', '.', '=', '!', '?', ';', ':']
+                    for punct in punctuation:
+                        line = line.replace(punct, '')
+                    line = line.replace(' - ', ' ')
+                    words.extend(line.split())
+
+                all_words[file_name] = words
         return all_words
 
     def find(self, word):
-        dict_ = {}
-        for name, words in self.get_all_words().items():
-            for w in words:
-                if word.lower() in w:
-                    index = words.index(w)
-                    dict_[self.file_name] = index + 1
-                    break
-        return dict_
+        places = {}
+        for key, value in self.get_all_words().items():
+            if word.lower() in value:
+                places[key] = value.index(word.lower()) + 1
+
+        return places
 
     def count(self, word):
-        dict_ = {}
-        count = 0
-        for name, words in self.get_all_words().items():
-            for w in words:
-                if word.lower() in w:
-                    count += 1
-        dict_[self.file_name] = count
-        return dict_
+        counters = {}
+        for value, key in self.get_all_words().items():
+            words_count = key.count(word.lower())
+            counters[value] = words_count
+
+        return counters
+
 
 finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
